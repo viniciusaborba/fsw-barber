fcca80a63215559afbed06ae4a165a74686c3d59
Vinícius Alves Borba <viniboal28@gmail.com>
2024-01-30 19:08:02 -0300

    feat: make welcome message component

---
+++ app/(home)/_components/welcome.tsx
@@ -0,0 +1,17 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+const WelcomeMessage = () => {
+  return (
+    <div className="px-5 pt-5 ">
+      <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+      <p className="capitalize text-sm">
+        {format(new Date(), "EEEE', 'dd 'de' MMMM", {
+          locale: ptBR,
+        })}
+      </p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
