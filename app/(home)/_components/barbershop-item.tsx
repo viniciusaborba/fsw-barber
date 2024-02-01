a33d0f6536c1848fec2a310b18981cc077f53e8b
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-01 11:34:52 -0300

    feat: add serviceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,15 +1,24 @@
+'use client'
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarberShopItemProps {
   barberShop: Barbershop;
 }
 
 const BarberShopItem = ({ barberShop }: BarberShopItemProps) => {
+  const router = useRouter()
+  
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barberShop.id}`)
+  }
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
@@ -44,7 +53,7 @@ const BarberShopItem = ({ barberShop }: BarberShopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {barberShop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
             Reservar
           </Button>
         </div>
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { db } from "@/app/_lib/prisma";
+import BarberShopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarberShopDetailsProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarberShopDetailsPage = async ({ params }: BarberShopDetailsProps) => {
+  if (!params.id) {
+    return null;
+  }
+
+  const barberShop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barberShop) {
+    return null;
+  }
+
+  return (
+    <div>
+      <BarberShopInfo barberShop={barberShop} />
+
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barberShop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarberShopDetailsPage;
