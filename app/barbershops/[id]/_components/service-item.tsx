9e5f82602920a9c64721e11f7fc9c5beac5bd6ef
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-08 10:00:49 -0300

    style: ensure footer is always on the bottom

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -172,7 +172,7 @@ const ServiceItem = ({
                     </Button>
                   </SheetTrigger>
 
-                  <SheetContent className="p-0">
+                  <SheetContent className="p-0 overflow-y-auto [&::-webkit-scrollbar]:hidden">
                     <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
                       Fazer Reserva
                     </SheetHeader>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
-import { Toaster } from "./_components/ui/sonner"; 
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
         <AuthProvider>
-          {children}
+          <div className="flex-1">{children}</div>
           <Toaster />
           <Footer />
         </AuthProvider>
