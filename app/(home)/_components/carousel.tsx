a73341f55a3ffc17446558d468408dbd637fe8a8
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-08 11:19:49 -0300

    feat: ensure popular barber shops are shown randomly

--- app/(home)/_components/carousel.tsx
+++ app/(home)/_components/carousel.tsx
@@ -3,10 +3,16 @@ import BarberShopItem from "./barbershop-item";
 
 interface CarouselProps {
   text: string;
+  popular?: boolean;
 }
 
-async function Carousel({ text }: CarouselProps) {
+async function Carousel({ text, popular }: CarouselProps) {
   const barberShops = await db.barbershop.findMany({});
+  const PopularBarberShops = await db.barbershop.findMany({
+    orderBy: {
+      id: "asc",
+    },
+  });
 
   return (
     <div className="mt-6">
@@ -14,13 +20,23 @@ async function Carousel({ text }: CarouselProps) {
         {text}
       </h2>
 
-      <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
-        {barberShops.map((barberShop) => (
-          <div key={barberShop.id} className="min-w-[167px] max-w-[167px]">
-            <BarberShopItem  barberShop={barberShop} />
-          </div>
-        ))}
-      </div>
+      {popular ? (
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+          {PopularBarberShops.map((barberShop) => (
+            <div key={barberShop.id} className="min-w-[167px] max-w-[167px]">
+              <BarberShopItem barberShop={barberShop} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+          {barberShops.map((barberShop) => (
+            <div key={barberShop.id} className="min-w-[167px] max-w-[167px]">
+              <BarberShopItem barberShop={barberShop} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -51,7 +51,7 @@ export default async function Home() {
 
       <Carousel text="Recomendados" />
 
-      <Carousel text="Populares" />
+      <Carousel text="Populares" popular/>
     </div>
   );
 }
